be9c853209f765508bf47a5a47128a28c29407d3
Thainara <tgabrielle14@gmail.com>
2024-12-02 18:03:24 -0300

    Modificações nas telas Home e Profile

--- plantare_app/lib/core/app_routes.dart
+++ plantare_app/lib/core/app_routes.dart
@@ -8,8 +8,12 @@ import '../screens/planting_screen.dart';
 import '../screens/report_screen.dart';
 import '../screens/community_screen.dart';
 import '../screens/comment_screen.dart';
+<<<<<<< HEAD
 import '../screens/profile.dart';
 
+=======
+import '../screens/profile_screen.dart';  // Importando a tela de perfil
+>>>>>>> 9fa6058 (Alterações na tela HomeScreen, novas rotas e adição da tela de perfil)
 
 class AppRoutes {
   static const String onboarding = '/';
@@ -21,7 +25,11 @@ class AppRoutes {
   static const String report = '/report';
   static const String community = '/community';
   static const String comment = '/comment';
+<<<<<<< HEAD
   static const String profile = '/profile';
+=======
+  static const String profile = '/profile';  // Adicionando a constante de rota para perfil
+>>>>>>> 9fa6058 (Alterações na tela HomeScreen, novas rotas e adição da tela de perfil)
 
   static final Map<String, WidgetBuilder> routes = {
     onboarding: (context) => OnboardingScreen(),
@@ -33,6 +41,10 @@ class AppRoutes {
     report: (context) => ReportScreen(),
     community: (context) => CommunityScreen(),
     comment: (context) => CommentScreen(),
+<<<<<<< HEAD
     profile: (context) => ProfileScreen(),
+=======
+    profile: (context) => ProfileScreen(),  // Registrando a tela de perfil
+>>>>>>> 9fa6058 (Alterações na tela HomeScreen, novas rotas e adição da tela de perfil)
   };
 }
--- plantare_app/lib/screens/profile_screen.dart
+++ plantare_app/lib/screens/profile_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import '../core/app_colors.dart';
-import '../core/app_text_styles.dart';
 
-class HomeScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +17,7 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Header (AppBar Customizada)
+            // Header
             Container(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
@@ -52,14 +59,14 @@ class HomeScreen extends StatelessWidget {
                     child: IconButton(
                       icon: Icon(Icons.arrow_back, color: Colors.grey),
                       onPressed: () {
-                        Navigator.pop(context); // Navegar para a tela anterior
+                        Navigator.pop(context);
                       },
                     ),
                   ),
                   Align(
                     alignment: Alignment.center,
                     child: Text(
-                      "OLÁ, O QUE VAMOS CULTIVAR?",
+                      "EDITAR PERFIL",
                       style: TextStyle(
                         fontFamily: 'Outfit',
                         fontSize: 20,
@@ -71,38 +78,85 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-
-            // Corpo da página com as opções de navegação
+            
+            // Campos de texto (Nome, E-mail, Telefone)
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // Opções de navegação com ícones do lado direito
-                  _buildOption(
-                    context,
-                    'Novo cultivo',
-                    'Prepare seu plantio',
-                    'assets/images/flower_icon_pink.png',
-                    '/planting', // Navegação para a página de plantio
+                  // Nome
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 16), // Espaço entre os botões
-
-                  _buildOption(
-                    context,
-                    'Dica e tutoriais',
-                    'Aprenda com especialistas',
-                    'assets/images/leaf_icon_green.png',
-                    '/community', // Navegação para página de tutoriais
+                  SizedBox(height: 16),
+                  // E-mail
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 16), // Espaço entre os botões
-
-                  _buildOption(
-                    context,
-                    'Comunidade',
-                    'Compartilhe e aprenda',
-                    'assets/images/leaf_icon_red.png',
-                    '/comment', // Navegação para a página de comunidade
+                  SizedBox(height: 16),
+                  // Telefone
+                  TextField(
+                    controller: phoneController,
+                    decoration: InputDecoration(
+                      labelText: 'Telefone',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  // Botões de salvar e cancelar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Ação de cancelar
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.amber, // Cor de fundo amarela
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 14, horizontal: 32),
+                        ),
+                        child: Text(
+                          'Cancelar',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Ação de salvar
+                          // Adicione a lógica para salvar os dados aqui
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF04BB86), // Cor de fundo verde
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 14, horizontal: 32),
+                        ),
+                        child: Text(
+                          'Salvar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -121,26 +175,26 @@ class HomeScreen extends StatelessWidget {
               IconButton(
                 icon: Icon(Icons.home),
                 onPressed: () {
-                  Navigator.pushNamed(context, '/home'); // Navegar para Home
+                  Navigator.pushNamed(context, '/home'); // Direciona para Home
                 },
               ),
               IconButton(
                 icon: Icon(Icons.people),
                 onPressed: () {
-                  Navigator.pushNamed(context, '/community'); // Navegar para Comunidade
+                  Navigator.pushNamed(context, '/community'); // Direciona para Comunidade
                 },
               ),
               SizedBox(width: 40), // Espaço para o botão central
               IconButton(
                 icon: Icon(Icons.analytics),
                 onPressed: () {
-                  Navigator.pushNamed(context, '/report'); // Navegar para Métricas
+                  Navigator.pushNamed(context, '/report'); // Direciona para Métricas
                 },
               ),
               IconButton(
                 icon: Icon(Icons.person),
                 onPressed: () {
-                  Navigator.pushNamed(context, '/profile'); // Navegar para Perfil
+                  Navigator.pushNamed(context, '/profile'); // Direciona para Perfil
                 },
               ),
             ],
@@ -160,7 +214,7 @@ class HomeScreen extends StatelessWidget {
         ),
         child: FloatingActionButton(
           onPressed: () {
-            Navigator.pushNamed(context, '/planting'); // Navegar para Plantio
+            Navigator.pushNamed(context, '/community'); // Direciona para a comunidade
           },
           backgroundColor: Colors.transparent,
           elevation: 0,
@@ -168,53 +222,6 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-
-  // Método auxiliar para criar os blocos de opções no corpo
-  Widget _buildOption(
-    BuildContext context,
-    String title,
-    String subtitle,
-    String iconPath,
-    String route,
-  ) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.9, // Largura ajustada
-        decoration: BoxDecoration(
-          color:Color(0xFFBFBFBF), // Fundo preto dos botões
-          borderRadius: BorderRadius.circular(20), // Borda arredondada
-        ),
-        child: ListTile(
-          contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-          title: Text(
-            title,
-            style: TextStyle(
-              fontFamily: 'Oswald', // Fonte Oswald para o título
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black, // Texto branco
-            ),
-          ),
-          subtitle: Text(
-            subtitle,
-            style: TextStyle(
-              fontFamily: 'Manrope', // Fonte Manrope para o subtítulo
-              color: Colors.black, // Texto do subtítulo mais claro
-              fontSize: 14,
-            ),
-          ),
-          trailing: Image.asset(
-            iconPath,
-            height: 40, // Ícone com tamanho fixo
-          ),
-          onTap: () {
-            Navigator.pushNamed(context, route); // Navegar ao clicar
-          },
-        ),
-      ),
     );
   }
 }
